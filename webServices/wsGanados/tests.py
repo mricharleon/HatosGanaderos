c3a9527d08f518b0febfc6ae9113f770a3d352dd
mricharleon <mricharleon@gmail.com>
2017-06-07 15:06:58 -0500

    Fixes - wsGanados.tests

--- webServices/wsGanados/tests.py
+++ webServices/wsGanados/tests.py
@@ -5,17 +5,18 @@ from django.test.client import Client
 
 from views import *
 
+
 class ProfileTestCase(TestCase):
     def setUp(self):
         pass
 
     def test_root_url_resolves_to_insemination_page_view(self):
-        found = resolve('/ajax/insemination/')  
-        self.assertEqual(found.func, insemination) 
+        found = resolve('/ajax/insemination/')
+        self.assertEqual(found.func, insemination)
 
     def test_insemination_page_returns_correct_html(self):
-        request = HttpRequest()  
-        response = insemination(request)  
-        self.assertTrue(response.content.startswith(b'<html>'))  
-        self.assertIn(b'<title>HatosGanaderos</title>', response.content)  
-        self.assertTrue(response.content.endswith(b'</html>')) 
+        request = HttpRequest()
+        response = insemination(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>HatosGanaderos</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
