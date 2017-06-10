bdfc080d02771a6e15e616d73d6abd5f91fb895a
mricharleon <mricharleon@gmail.com>
2017-06-10 09:26:18 -0500

    Fixes2 - notificaciones.tests

--- notifications/tests.py
+++ notifications/tests.py
@@ -9,15 +9,15 @@ from models import Notification
 class NotificationTestCase(TestCase):
     def setUp(self):
         Notification.objects.create(star_date='2014-10-01',
-            end_date='2014-10-10',
-            state=2,
-            module=0,
-            ident_cattle=1,
-            ident_sperm=1,
-            ident_medicament=1,
-            ident_food=1,
-            name=0,
-            farm=1)
+                end_date='2014-10-10',
+                state=2,
+                module=0,
+                ident_cattle=1,
+                ident_sperm=1,
+                ident_medicament=1,
+                ident_food=1,
+                name=0,
+                farm=1)
         print "inicio el setup()"
 
     def listaNotificacionesReproduccion(self):
